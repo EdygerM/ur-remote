0b7cfc302b04970a0d178ec6fc4089942b0ab0bb
Edy Mariano <edy.mariano1@gmail.com>
2022-10-11 11:03:38 +0200

    add README

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# UR Dashboard
-A library in progress to control a Universal Robot using the Dashboard server
+# ur-remote
+A library in progress to control a Universal Robot in remote control
 
 ### Installation
 ``
-pip install ur-dashboard
+pip install ur-remote
 ``
 
 ### Get started
 ```Python
-from ur_dashboard import URDashboard
 
-robot1 = URDashboard("127.0.0.1")
-
-print(robot1.connect())
 ```
---
+++ setup.py
@@ -0,0 +1,34 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="ur-remote",
+    version="0.1.1",
+    description="Library allowing to remote control a Universal Robot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://ur-remote.readthedocs.io/",
+    author="Joffrey Bienvenu",
+    author_email="edy.mariano1@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent"
+    ],
+    packages=["ur-remote"],
+    include_package_data=True,
+)
