a4d54677c2f6586e5f9e481e69444dfa0021a52a
Edy Mariano <edy.mariano1@gmail.com>
2022-10-19 15:59:13 +0200

    Complete Primary Class

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from ur_remote.URRobot import URRobot
 
 if __name__ == '__main__':
     robot1 = URRobot("172.31.0.101")
-    robot1.Dashboard.connect()
-    robot1.Primary.connect()
+    print(robot1.Dashboard.connect())
+    print(robot1.Primary.connect())
