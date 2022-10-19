35a6ba344a0c431812143a644874d7c6c8cd9dde
Edy Mariano <edy.mariano1@gmail.com>
2022-10-19 17:12:21 +0200

    Test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     robot1 = URRobot("172.31.0.101")
     print(robot1.Dashboard.connect())
     print(robot1.Dashboard.load("moveAToB"))
+    print(robot1.Dashboard.powerOffRobotArm())
     # print(robot1.Dashboard.isRunning())
     # print(robot1.Dashboard.getRobotMode())
     print(robot1.Dashboard.getLoadedProgram())
