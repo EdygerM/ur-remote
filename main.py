928a95f1e40dd6e8139c61b0986b65ba068f92a4
Edy Mariano <edy.mariano1@gmail.com>
2022-10-19 17:02:24 +0200

    Test

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from ur_remote.URRobot import URRobot
 if __name__ == '__main__':
     robot1 = URRobot("172.31.0.101")
     print(robot1.Dashboard.connect())
-    print(robot1.Dashboard.shutdown())
-    print(robot1.Dashboard.load("MoveAToB"))
+    print(robot1.Dashboard.load("moveAToB"))
     print(robot1.Dashboard.isRunning())
     print(robot1.Dashboard.getRobotMode())
     print(robot1.Dashboard.getLoadedProgram())
